9a4b6fd619324f7f9063985937c702f938bbc5b0
Matheuscruztj <matheus.cbmdk@gmail.com>
2018-06-25 22:31:44 -0300

    Commit 1.0
    VK Translator 1.0

---
+++ src/vktranslator/Letras.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vktranslator;
+
+/**
+ *
+ * @author Administrador
+ */
+public class Letras {
+    public String encode;
+    public String traduzido;
+
+    /**
+     * @return the encode
+     */
+    public String getEncode() {
+        return encode;
+    }
+
+    /**
+     * @param encode the encode to set
+     */
+    public void setEncode(String encode) {
+        this.encode = encode;
+    }
+
+    /**
+     * @return the traduzido
+     */
+    public String getTraduzido() {
+        return traduzido;
+    }
+
+    /**
+     * @param traduzido the traduzido to set
+     */
+    public void setTraduzido(String traduzido) {
+        this.traduzido = traduzido;
+    }
+}
